6ace258da858a19de3a93fbd7ef52557bb226f7c
Suleman-Arshad <sulemanarshad0682@gmail.com>
2026-08-08 14:21:54 +0500

    feat(reconstruction): add VolumeReconstructor interface
    Declare reconstruct() and reconstructParallel(), which stack a slice
    series into a VoxelVolume via HU normalization and trilinear
    interpolation, with a thread-pool-parallelized resampling variant.

--- include/dicom_processor/reconstruction.hpp
+++ include/dicom_processor/reconstruction.hpp
@@ -1,22 +1,21 @@
 #pragma once
 #include "dicom_processor/parser.hpp"
+#include "dicom_processor/thread_pool.hpp"
 #include "dicom_processor/voxel_volume.hpp"
-
 #include <stdexcept>
 #include <vector>
 
-namespace dicom
-{
+namespace dicom {
 
-    struct ReconstructionError : std::runtime_error
-    {
-        using std::runtime_error::runtime_error;
-    };
+struct ReconstructionError : std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
 
-    class VolumeReconstructor
-    {
-    public:
-        static VoxelVolume reconstruct(std::vector<Slice> slices, double targetSpacingMM = -1.0);
-    };
+class VolumeReconstructor {
+public:
+    static VoxelVolume reconstruct(std::vector<Slice> slices, double targetSpacingMM = -1.0);
+    static VoxelVolume reconstructParallel(std::vector<Slice> slices, ThreadPool& pool,
+                                            double targetSpacingMM = -1.0);
+};
 
-} // namespace dicom
+}  // namespace dicom
